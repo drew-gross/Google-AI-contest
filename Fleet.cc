ce104beaaab4f8eef67f30c9605b38cf8e0a25d9
Drew <dooey22@hotmail.com>
2010-10-17 16:20:18 -0600

    Changed spaces to tabs

--- Fleet.cc
+++ Fleet.cc
@@ -1,39 +1,39 @@
 #include "Fleet.h"
 
 Fleet::Fleet(int owner,
-             int num_ships,
-             int source_planet,
-             int destination_planet,
-             int total_trip_length,
-             int turns_remaining) {
-  owner_ = owner;
-  num_ships_ = num_ships;
-  source_planet_ = source_planet;
-  destination_planet_ = destination_planet;
-  total_trip_length_ = total_trip_length;
-  turns_remaining_ = turns_remaining;
+	int num_ships,
+	int source_planet,
+	int destination_planet,
+	int total_trip_length,
+	int turns_remaining) {
+		owner_ = owner;
+		num_ships_ = num_ships;
+		source_planet_ = source_planet;
+		destination_planet_ = destination_planet;
+		total_trip_length_ = total_trip_length;
+		turns_remaining_ = turns_remaining;
 }
 
 int Fleet::Owner() const {
-  return owner_;
+	return owner_;
 }
 
 int Fleet::NumShips() const {
-  return num_ships_;
+	return num_ships_;
 }
 
 int Fleet::SourcePlanet() const {
-  return source_planet_;
+	return source_planet_;
 }
 
 int Fleet::DestinationPlanet() const {
-  return destination_planet_;
+	return destination_planet_;
 }
 
 int Fleet::TotalTripLength() const {
-  return total_trip_length_;
+	return total_trip_length_;
 }
 
 int Fleet::TurnsRemaining() const {
-  return turns_remaining_;
+	return turns_remaining_;
 }
--- Fleet.h
+++ Fleet.h
@@ -4,45 +4,45 @@
 // This class stores details about one fleet. There is one of these classes
 // for each fleet that is in flight at any given time.
 class Fleet {
- public:
-  // Initializes a fleet.
-  Fleet(int owner,
-        int num_ships,
-        int source_planet = -1,
-        int destination_planet = -1,
-        int total_trip_length = -1,
-        int turns_remaining = -1);
+public:
+	// Initializes a fleet.
+	Fleet(int owner,
+		int num_ships,
+		int source_planet = -1,
+		int destination_planet = -1,
+		int total_trip_length = -1,
+		int turns_remaining = -1);
 
-  // Returns the playerID of the owner of the fleet. Your player ID is always
-  // 1. So if the owner is 1, you own the fleet. If the owner is 2 or some
-  // other number, then this fleet belongs to your enemy.
-  int Owner() const;
+	// Returns the playerID of the owner of the fleet. Your player ID is always
+	// 1. So if the owner is 1, you own the fleet. If the owner is 2 or some
+	// other number, then this fleet belongs to your enemy.
+	int Owner() const;
 
-  // Returns the number of ships that comprise this fleet.
-  int NumShips() const;
+	// Returns the number of ships that comprise this fleet.
+	int NumShips() const;
 
-  // Returns the ID of the planet where this fleet originated.
-  int SourcePlanet() const;
+	// Returns the ID of the planet where this fleet originated.
+	int SourcePlanet() const;
 
-  // Returns the ID of the planet where this fleet is headed.
-  int DestinationPlanet() const;
+	// Returns the ID of the planet where this fleet is headed.
+	int DestinationPlanet() const;
 
-  // Returns the total distance that is being traveled by this fleet. This
-  // is the distance between the source planet and the destination planet,
-  // rounded up to the nearest whole number.
-  int TotalTripLength() const;
+	// Returns the total distance that is being traveled by this fleet. This
+	// is the distance between the source planet and the destination planet,
+	// rounded up to the nearest whole number.
+	int TotalTripLength() const;
 
-  // Returns the number of turns until this fleet reaches its destination. If
-  // this value is 1, then the fleet will hit the destination planet next turn.
-  int TurnsRemaining() const;
+	// Returns the number of turns until this fleet reaches its destination. If
+	// this value is 1, then the fleet will hit the destination planet next turn.
+	int TurnsRemaining() const;
 
- private:
-  int owner_;
-  int num_ships_;
-  int source_planet_;
-  int destination_planet_;
-  int total_trip_length_;
-  int turns_remaining_;
+private:
+	int owner_;
+	int num_ships_;
+	int source_planet_;
+	int destination_planet_;
+	int total_trip_length_;
+	int turns_remaining_;
 };
 
 #endif //FLEET_H
